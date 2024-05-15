e708baf8ba97833ccb9694eb6c57a1ba7d2aaa01
Rangga <rangga.yudhistira@ui.ac.id>
2024-05-16 00:32:00 +0700

    Initial Commit

---
+++ DokiDokiRace Watch App/DokiDokiRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DokiDokiRaceApp.swift
+//  DokiDokiRace Watch App
+//
+//  Created by Rangga Yudhistira Brata on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DokiDokiRace_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DokiDokiRace/DokiDokiRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DokiDokiRaceApp.swift
+//  DokiDokiRace
+//
+//  Created by Rangga Yudhistira Brata on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DokiDokiRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
